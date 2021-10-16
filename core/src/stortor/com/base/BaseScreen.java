580ad6f9578014b41db582cdbcf60a8f4771d58f
stortor <storrtor@gmail.com>
2021-10-16 19:27:19 +0300

    Class work 2

---
+++ core/src/stortor/com/base/BaseScreen.java
@@ -0,0 +1,98 @@
+package stortor.com.base;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class BaseScreen implements Screen, InputProcessor {
+
+    protected SpriteBatch batch;
+
+    @Override
+    public void show() {
+        System.out.println("show");
+        batch = new SpriteBatch();
+        Gdx.input.setInputProcessor(this);
+    }
+
+    @Override
+    public void render(float delta) {
+        ScreenUtils.clear(0.1f,0.5f,0.3f,1);
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        System.out.println("resize w = " + width + " h = " + height);
+    }
+
+    @Override
+    public void pause() {
+        System.out.println("pause");
+    }
+
+    @Override
+    public void resume() {
+        System.out.println("resume");
+    }
+
+    @Override
+    public void hide() {
+        System.out.println("hide");
+        dispose();
+    }
+
+    @Override
+    public void dispose() {
+        System.out.println("dispose");
+        batch.dispose();
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        System.out.println("keyDown keycode = " + keycode);
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        System.out.println("keyUp keycode = " + keycode);
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        System.out.println("keyTyped charter = " + character);
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        System.out.println("touchDown screenX = " + screenX + " screenY " + screenY);
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        System.out.println("touchUp screenX = " + screenX + " screenY " + screenY);
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        System.out.println("touchDragged screenX = " + screenX + " screenY " + screenY);
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(float amountX, float amountY) {
+        System.out.println("scrolled amountX = " + amountX + " amountY " + amountY);
+        return false;
+    }
+}
